fb329d5aadec1c61f79d9d66a5f3859ea1f27dc1
Maria Sharabayko <msharabayko@haivision.com>
2021-10-19 18:27:50 +0200

    Saved intermediate work

--- examples/srt/srt-server.py
+++ examples/srt/srt-server.py
@@ -1,33 +1,82 @@
-import socket
-import sys
 import argparse
+from enum import Enum
+import socket
+
+import attr
+
 
 HOST = 'localhost'
-RECV_BUF_SIZE = 1500  # Corresponds to MTU
+RECV_BUF_SIZE = 1500  # Corresponds to Ethernet MTU
+
+
+class HandshakeType(Enum):
+    INDUCTION = b'\x00\x00\x00\x01'
+
+
+@attr.s
+class SrtHandshake(object):
+    """ SRT Handshake Packet """
+    # TODO: Validator for the first two bytes
+    header: bytes = attr.ib()
+    payload: bytes = attr.ib()
+
+    timestamp: bytes = attr.ib()
+    @timestamp.default
+    def _extract_timestamp(self):
+        return int.from_bytes(self.header[8:12], "big")
+
+    version: int = attr.ib()
+    @version.default
+    def _extract_version(self):
+        return int.from_bytes(self.payload[:4], "big")
+
+    # ??? long int
+    initial_seqno: int = attr.ib()
+    @initial_seqno.default
+    def _extract_initial_seqno(self):
+        return int.from_bytes(self.payload[8:12], "big")
+
+    type: HandshakeType = attr.ib()
+    @type.default
+    def _extract_type(self):
+        return HandshakeType(self.payload[20:24])
+
+    srt_socket_id: bytes = attr.ib()
+    @srt_socket_id.default
+    def _extract_srt_socket_id(self):
+        return self.payload[24:28]
 
 
 def srt_server(port): 
     """ A simple SRT server """
 
-    # Create a UDP socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+        sock.bind((HOST, port))
 
-    # Bind the socket to address and port
-    sock.bind((HOST, port))
+        print(f"SRT server up and listening on {HOST} port {port}")
 
-    print(f"SRT server up and listening on {HOST} port {port}")
+        # Listen for incoming datagrams
+        while True:
+            data, addr = sock.recvfrom(RECV_BUF_SIZE)
 
-    # Listen for incoming datagrams
-    while True:
-        data, addr = sock.recvfrom(RECV_BUF_SIZE)
+            if not data: # recvfrom() returns b''
+                # TODO: Doesn't work, hanging on recvfrom() if there is no incoming data
+                break
 
-        print (f"Received {len(data)} bytes from {addr}")
-        print (f"Data: {data}")
+            print (f"Received {len(data)} bytes from {addr}")
+            print (f"Data: {data}")
 
-        # Detect incoming handshake
-        if data.startswith(b'\x80\x00'):
-            print(f'This is an SRT handshake. Timestamp: {data[8:12]} ~ {int.from_bytes(data[8:12], "big")} \n')
-            # break
+            # Detect incoming handshake
+            if data.startswith(b'\x80\x00'):
+                # print(f'This is an SRT handshake. Timestamp: {data[8:12]} ~ {int.from_bytes(data[8:12], "big")} \n')
+
+                hs = SrtHandshake(data[:16], data[16:])
+                print(hs)
+
+                if hs.type == HandshakeType.INDUCTION:
+                    print("Induction handshake, break")
+                    
+                    # TODO: reply to handshake
 
 
 if __name__ == '__main__':
